c6e67c63671dea12b90e81678ce3a99fabe50f8b
njacquemin1993 <n.jacquemin1993@gmail.com>
2019-12-29 18:31:38 +0100

    Download an example youtube video

--- app/src/main/java/com/njacquemin/youtubedl/MainActivity.kt
+++ app/src/main/java/com/njacquemin/youtubedl/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import com.chaquo.python.android.AndroidPlatform
 import com.chaquo.python.Python
+import com.chaquo.python.PyObject
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +18,13 @@ class MainActivity : AppCompatActivity() {
         Python.start(AndroidPlatform(this))
 
         val py = Python.getInstance()
-        val os = py.getModule("os")
-        val path = os["path"]?.callAttr("join", "test", "path")
-        Log.e("MAIN", path.toString())
+
+        val d = py.builtins.callAttr("dict")
+        d.callAttr("__setitem__", "outtmpl", "sdcard/%(title)s-%(id)s.%(ext)s")
+
+        val yt = py.getModule("youtube_dl")
+        val ydl = yt.callAttr("YoutubeDL", d)
+        ydl.callAttr("download", arrayOf("https://www.youtube.com/watch?v=BaW_jenozKc"))
+        Log.e("MAIN", "----------------- DONE ----------------")
     }
 }
