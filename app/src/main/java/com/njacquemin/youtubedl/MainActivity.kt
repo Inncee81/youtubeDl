b338994fd5033eca0e7928a506c4676407f5b597
njacquemin1993 <n.jacquemin1993@gmail.com>
2019-12-30 22:05:32 +0100

    Add basic table to see content

--- app/src/main/java/com/njacquemin/youtubedl/MainActivity.kt
+++ app/src/main/java/com/njacquemin/youtubedl/MainActivity.kt
@@ -5,12 +5,26 @@ import android.os.Bundle
 import java.io.File
 import android.content.Intent
 import android.util.Log
+import android.widget.TableRow
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+private fun layoutParam(weight: Float): TableRow.LayoutParams {
+    return TableRow.LayoutParams(
+        0,
+        TableRow.LayoutParams.MATCH_PARENT,
+        weight
+    )
+}
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        addRow("1", "1", true)
+//        addRow("1iuhsiusahuioasNAsisa", "iuhsadiubdsauidsa", false)
 
         val yt = YoutubeDownloader(this, null)
         yt.download("https://www.youtube.com/watch?v=BaW_jenozKc", File("sdcard", "test.mp3"))
@@ -37,5 +51,32 @@ class MainActivity : AppCompatActivity() {
         } else {
             finish()
         }*/
+    }
+
+    private fun addRow(link: String, name: String, downloaded: Boolean) {
+        val background = if (table.childCount % 2 == 1) R.drawable.content_cell_even else R.drawable.content_cell_odd
+        val newRow = TableRow(applicationContext)
+        val linkView = TextView(applicationContext).apply {
+            text = link
+            layoutParams = layoutParam(2.0f)
+            setBackgroundResource(background)
+            setPadding(10, 0, 0,0)
+        }
+        val nameView = TextView(applicationContext).apply {
+            text = name
+            layoutParams = layoutParam(3.0f)
+            setBackgroundResource(background)
+            setPadding(10, 0, 0,0)
+        }
+        val buttonView = TextView(applicationContext).apply {
+            text = if (downloaded) "Yes" else "No"
+            layoutParams = layoutParam(1.0f)
+            setBackgroundResource(background)
+            setPadding(10, 0, 0,0)
+        }
+        newRow.addView(linkView)
+        newRow.addView(nameView)
+        newRow.addView(buttonView)
+        table.addView(newRow)
     }
 }
