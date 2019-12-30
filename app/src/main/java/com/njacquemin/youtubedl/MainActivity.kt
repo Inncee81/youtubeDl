dca333443593877d84c2f798e0c9a1b5ac5baabd
njacquemin1993 <n.jacquemin1993@gmail.com>
2019-12-30 20:23:57 +0100

    Allow to share link to app

--- app/src/main/java/com/njacquemin/youtubedl/MainActivity.kt
+++ app/src/main/java/com/njacquemin/youtubedl/MainActivity.kt
@@ -3,6 +3,8 @@ package com.njacquemin.youtubedl
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import java.io.File
+import android.content.Intent
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,28 @@ class MainActivity : AppCompatActivity() {
 
         val yt = YoutubeDownloader(this, null)
         yt.download("https://www.youtube.com/watch?v=BaW_jenozKc", File("sdcard", "test.mp3"))
+
+        // Check how it was started and if we can get the youtube link
+        Log.e("YTDOWNLOADER", "check...")
+        if (savedInstanceState == null && Intent.ACTION_SEND == intent.action
+            && intent.type != null && "text/plain" == intent.type
+        ) {
+
+            val ytLink = intent.getStringExtra(Intent.EXTRA_TEXT)
+            Log.e("YTDOWNLOADER", "Got shared link: $ytLink")
+
+            /*if (ytLink != null && (ytLink.contains("://youtu.be/") || ytLink.contains("youtube.com/watch?v="))) {
+                youtubeLink = ytLink
+                // We have a valid link
+                getYoutubeDownloadUrl(youtubeLink)
+            } else {
+                Toast.makeText(this, "No yt link", Toast.LENGTH_LONG).show()
+                finish()
+            }*/
+        } /*else if (savedInstanceState != null && youtubeLink != null) {
+            getYoutubeDownloadUrl(youtubeLink)
+        } else {
+            finish()
+        }*/
     }
 }
